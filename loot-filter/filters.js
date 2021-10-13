ecc8e883b28a0e486769d0a41532dc8d353ee720
Nick Ng <nicholas.cj.ng@gmail.com>
2021-10-13 20:37:40 +1300

    Show linked body armours

--- loot-filter/filters.js
+++ loot-filter/filters.js
@@ -222,16 +222,20 @@ Show
     make3LinkFilter("BBB", "Boots", "3b boots"),
     make3LinkFilter("BBB", "Gloves", "3b gloves"),
     make3LinkFilter("BBB", "Helmets", "3b helm"),
+    make3LinkFilter("BBB", "Body Armours", "3b body"),
     make3LinkFilter("BBG", "Wand", "2b1g wand"),
     make3LinkFilter("BBG", "Boots", "2b1g boots"),
     make3LinkFilter("BBG", "Gloves", "2b1g gloves"),
     make3LinkFilter("BBG", "Helmets", "2b1g helm"),
+    make3LinkFilter("BBG", "Body Armours", "2b1g body"),
     make3LinkFilter("BGG", "Boots", "2g1b boots"),
     make3LinkFilter("BGG", "Gloves", "2g1b gloves"),
     make3LinkFilter("BGG", "Helmets", "2g1b helm"),
+    make3LinkFilter("BGG", "Body Armours", "2g1b body"),
     make3LinkFilter("GGG", "Boots", "3g boots"),
     make3LinkFilter("GGG", "Gloves", "3g gloves"),
     make3LinkFilter("GGG", "Helmets", "3g helm"),
+    make3LinkFilter("GGG", "Body Armours", "3g body"),
   ].join("\n");
 };
 
