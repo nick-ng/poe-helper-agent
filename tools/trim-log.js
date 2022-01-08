b5978fcc3a8570d9dc718e92aa5e7ac623f3ce42
Nick Ng <nicholas.cj.ng@gmail.com>
2022-01-08 16:22:14 +1300

    tweak shield leveling filter and filter log before trimming

--- tools/trim-log.js
+++ tools/trim-log.js
@@ -8,6 +8,36 @@ import {
 import { join } from "path";
 
 const LOG_FILENAME = "Client.txt";
+const MAX_LOG_LENGTH = 10000;
+const IGNORE_REGEX = [
+  /\] \[vulkan\]/i,
+  /\] \[job\]/i,
+  /\] \[startup\]/i,
+  /\] \[bundle.*\]/i,
+  /\] \[shader\]/i,
+  /\] \[engine\]/i,
+  /\] \[device\]/i,
+  /\] \[sound\]/i,
+  /\] \[eos sdk\]/i,
+  /\[debug client /i,
+  /\*{5} log file opening \*{5}/i,
+  /\.pathofexile\.com/i,
+  /\] connecting to instance server at (\d{1,3}\.){3}\d{1,3}:\d+$/i,
+  /\] tile hash: \d+$/i,
+  /\] doodad hash: \d+$/i,
+  /\] send patching protocol version \d+$/i,
+  /\] precalc\w+$/i,
+  /\] Enumerated adapter: /i,
+  /\] Requesting root contents/i,
+  /\] Got file list for "/i,
+  /\] Requesting folder Redist/i,
+  /\] Finished checking files$/i,
+  /\] Changing to device "/i,
+  /\] Generated Entitlement Data$/i,
+  /\] Redeeming \d+ Epic Entitlements$/i,
+  /\] : Redeemed Entitlements Successfully$/i,
+  /\] Generated Offer Data$/i,
+];
 
 function trimLog(logDir) {
   const logFilePath = join(logDir, LOG_FILENAME);
@@ -17,12 +47,28 @@ function trimLog(logDir) {
   if (fileStats) {
     const logContents = readFileSync(logFilePath).toString().split("\r\n");
 
-    if (logContents.length <= 1000) {
+    const logContents2 = logContents
+      .filter((a) => {
+        for (const r of IGNORE_REGEX) {
+          if (a.match(r)) {
+            return false;
+          }
+        }
+
+        return true;
+      })
+      .slice(-MAX_LOG_LENGTH);
+
+    if (logContents.length <= logContents2.length) {
       return;
     }
 
-    console.info(`Trimming ${logFilePath}. Had ${logContents.length} lines.`);
-    const newLogContents = `${logContents.slice(-1000).join("\r\n")}`;
+    console.info(
+      `Trimmed ${
+        logContents.length - logContents2.length
+      } lines from ${logFilePath}.`
+    );
+    const newLogContents = `${logContents2.join("\r\n")}`;
     writeFileSync(logFilePath, newLogContents);
   }
 }
