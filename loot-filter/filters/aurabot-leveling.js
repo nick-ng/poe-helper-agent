6dcd54639ef277f373d5992b2600c85d9496f914
Nick Ng <nicholas.cj.ng@gmail.com>
2022-02-01 18:17:56 +1300

    change aurabot to lightning trap leveling

--- loot-filter/filters/aurabot-leveling.js
+++ loot-filter/filters/aurabot-leveling.js
@@ -38,30 +38,34 @@ const bows = [
   "Maraketh Bow",
 ];
 
-const custom = `Show
-  SetBorderColor 200 0 0
-  SetFontSize 45
-  ItemLevel >= 1
-  BaseType "Rustic Sash"
-  ItemLevel <= 66
-  CustomAlertSound "sounds/rustic sash.mp3"
+const custom = `
+#Show
+#  SetBorderColor 200 0 0
+#  SetFontSize 45
+#  ItemLevel >= 1
+#  BaseType "Rustic Sash"
+#  ItemLevel <= 66
+#  CustomAlertSound "sounds/rustic sash.mp3"
+
+Show  # %HS3 $type->divination $tier->t4
+	Class "Divination"
+	BaseType == "Thunderous Skies"
+	SetFontSize 45
+	SetTextColor 255 0 255 255
+	SetBackgroundColor 39 141 192 255
+	SetBorderColor 0 0 0 255
+	MinimapIcon 2 White Triangle
+	PlayAlertSound 16 200
+  MinimapIcon 1 Pink Star
 
 Show
-  BaseType "Royal Bow" "Crude Bow" "Short Bow" "Imperial Bow"
+  BaseType "Long Bow" "Royal Bow" "Crude Bow" "Short Bow" "Imperial Bow"
   Rarity Unique
   SetFontSize 45
   SetBackgroundColor 255 0 255 255
   SetBorderColor 255 255 255 255
   PlayAlertSound 16 200
   MinimapIcon 1 Pink Star
-
-Show
-  BaseType "Royal Bow" "Crude Bow" "Short Bow" "Imperial Bow"
-  Rarity Unique
-  SetFontSize 35
-  SetBackgroundColor 255 0 255 255
-  SetBorderColor 214 126 0 255
-  MinimapIcon 1 Pink Cross
 
 Show
   AreaLevel <= 75
@@ -78,11 +82,11 @@ Show
 const weapons = [
   [12, "Longsword", true],
   [18, "Woodsplitter", true],
-  [18, "Bastard Sword", true],
-  [23, "Poleaxe", true],
-  [33, "Double Axe", true],
-  [37, "Gilded Axe", true],
-  [41, "Shadow Axe", true],
+  // [18, "Bastard Sword", true],
+  // [23, "Poleaxe", true],
+  // [33, "Double Axe", true],
+  // [37, "Gilded Axe", true],
+  // [41, "Shadow Axe", true],
   // [45, "Jasper Chopper", true],
   // [45, "Dagger Axe", true],
   // [45, "Timber Axe", false],
@@ -115,8 +119,8 @@ export default function getFilter() {
     }),
 
     // 4-Links
-    make4LinkFilter("GGGR", "3g1r", 1, 62),
-    make4LinkFilter("RRGG", "2r2g", 1, 40),
+    // make4LinkFilter("GGGR", "3g1r", 1, 62),
+    make4LinkFilter("RRGG", "2r2g", 1, 62),
     make4LinkFilter("RRBB", "2b2r", 1, 62),
     // make4LinkFilter("RRBG", "2rgb", 1, 62),
     // make4LinkFilter("RBBG", "2rgb", 1, 62),
@@ -126,10 +130,10 @@ export default function getFilter() {
     make4LinkFilter("BBGG", "2b2g", 1, 62),
 
     // 3-Links
-    make3LinkFilter("GGR", "Boots", "2g1r boots"),
-    make3LinkFilter("GGR", "Gloves", "2g1r gloves"),
-    make3LinkFilter("GGR", "Helmets", "2g1r helm"),
-    make3LinkFilter("GGR", "Body Armours", "2g1r body"),
+    make3LinkFilter("GGR", "Boots", "2g1r boots", 1, 12),
+    make3LinkFilter("GGR", "Gloves", "2g1r gloves", 1, 12),
+    make3LinkFilter("GGR", "Helmets", "2g1r helm", 1, 12),
+    make3LinkFilter("GGR", "Body Armours", "2g1r body", 1, 12),
     make3LinkFilter("BBR", "Wand", "2b1r"),
     make3LinkFilter("BBR", "Boots", "2b1r", 1, 62),
     make3LinkFilter("BBR", "Gloves", "2b1r", 1, 62),
@@ -140,15 +144,15 @@ export default function getFilter() {
     // make3LinkFilter("BRR", "Gloves", "2r1b", 35, 62),
     // make3LinkFilter("BRR", "Helmets", "2r1b", 35, 62),
     // make3LinkFilter("BRR", "Body Armours", "2r1b", 35, 62),
-    make3LinkFilter("BBG", "Wand", "2b1g wand", 8, 62),
-    make3LinkFilter("BBG", "Boots", "2b1g boots", 8, 62),
-    make3LinkFilter("BBG", "Gloves", "2b1g gloves", 8, 62),
-    make3LinkFilter("BBG", "Helmets", "2b1g helm", 8, 62),
-    make3LinkFilter("BBG", "Body Armours", "2b1g body", 8, 62),
-    make3LinkFilter("BGG", "Boots", "2g1b boots", 8, 62),
-    make3LinkFilter("BGG", "Gloves", "2g1b gloves", 8, 62),
-    make3LinkFilter("BGG", "Helmets", "2g1b helm", 8, 62),
-    make3LinkFilter("BGG", "Body Armours", "2g1b body", 8, 62),
+    make3LinkFilter("BBG", "Wand", "2b1g wand", 1, 62),
+    make3LinkFilter("BBG", "Boots", "2b1g boots", 1, 62),
+    make3LinkFilter("BBG", "Gloves", "2b1g gloves", 1, 62),
+    make3LinkFilter("BBG", "Helmets", "2b1g helm", 1, 62),
+    make3LinkFilter("BBG", "Body Armours", "2b1g body", 1, 62),
+    make3LinkFilter("BGG", "Boots", "2g1b boots", 1, 62),
+    make3LinkFilter("BGG", "Gloves", "2g1b gloves", 1, 62),
+    make3LinkFilter("BGG", "Helmets", "2g1b helm", 1, 62),
+    make3LinkFilter("BGG", "Body Armours", "2g1b body", 1, 62),
 
     // Other People
     // make3LinkFilter("BBB", "Wand", "ding"),
@@ -159,8 +163,8 @@ export default function getFilter() {
     make3LinkFilterB("BGG", "ding"),
 
     // other stuff
-    makeColourFilter("RR"),
-    makeColourFilter("RG"),
+    // makeColourFilter("RR"),
+    // makeColourFilter("RG"),
     levelingCurrencyFilter,
   ].join("\n\n");
 }
