a04d9a4034b75f0e945ca2848cd6f691f12ea1d1
Nick Ng <nicholas.cj.ng@gmail.com>
2022-04-17 14:11:34 +1200

    refactored ggg filter fetching code

--- loot-filter/update-base-filters.js
+++ loot-filter/update-base-filters.js
@@ -11,32 +11,36 @@ import { writeFileSync } from "./write-filters.js";
  * 20: alch & go
  * 50: nem 3
  */
-const FILTERS = [
+const FILTER_BLAST = [
   {
     group: "neversink",
     url: "https://filterblast.xyz/api/FilterFile/?filter=NeverSink&colortheme=Normal&soundtheme=Default",
     presets: [
       { filename: "nsss.filter", preset: "SEMI-STRICT", level: 10 },
       { filename: "nsvs.filter", preset: "VERY-STRICT", level: 20 },
-      { filename: "nsub.filter", preset: "UBER-STRICT", level: 45 },
-      { filename: "nsup.filter", preset: "UBER-PLUS-STRICT", level: 50 },
+      // { filename: "nsub.filter", preset: "UBER-STRICT", level: 45 },
+      // { filename: "nsup.filter", preset: "UBER-PLUS-STRICT", level: 50 },
     ],
   },
 ];
 
+const GGG_FILTERS = [
+  // {
+  //   filename: "nsvs-l.filter",
+  //   preset: "neversink-very-strict-sc-live",
+  //   filterUrl: "https://www.pathofexile.com/item-filter/GmXoTD",
+  //   level: 19,
+  // },
+  // {
+  //   filename: "nsub-l.filter",
+  //   preset: "neversink-uber-strict-sc-live",
+  //   filterUrl: "https://www.pathofexile.com/item-filter/wvPeTJ",
+  //   level: 44,
+  // },
+];
+
 export const presets = _.flatMap(
-  [
-    ...FILTERS,
-    {
-      presets: [
-        {
-          filename: "custom.filter",
-          presets: "a",
-          level: 46,
-        },
-      ],
-    },
-  ],
+  [...FILTER_BLAST, { presets: GGG_FILTERS }],
   (filterGroup) => filterGroup.presets
 );
 
@@ -46,40 +50,31 @@ const fetchAndSaveFilter = async (fullUrl, filename) => {
   writeFileSync(resolve(".", "base-filters", filename), filterText);
 };
 
-export const fetchAndSaveFilters = async () => {
-  console.info("Fetching filters");
-  for (const filter of FILTERS) {
-    for (const preset of filter.presets) {
-      console.info(`Fetching ${filter.group} ${preset.preset}`);
-      const fullUrl = `${filter.url}&preset=${preset.preset}`;
-      await fetchAndSaveFilter(fullUrl, `${preset.level}${preset.filename}`);
-    }
-  }
-
-  if (process.env.POESESSID) {
+const fetchGggFilter = async (
+  poesessid,
+  { filename, preset, filterUrl, level }
+) => {
+  if (poesessid) {
     try {
-      console.info("Fetching NeverSink-5uberstr-softcore");
-      const res = await fetch(
-        "https://www.pathofexile.com/item-filter/wvPeTJ",
-        {
-          credentials: "include",
-          headers: {
-            Accept:
-              "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-            "Accept-Language": "en-GB,en;q=0.5",
-            Cookie: `POESESSID=${process.env.POESESSID}`,
-            "Upgrade-Insecure-Requests": "1",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "same-origin",
-            "Sec-Fetch-User": "?1",
-            Pragma: "no-cache",
-            "Cache-Control": "no-cache",
-          },
-          method: "GET",
-          mode: "cors",
-        }
-      );
+      console.info(`Fetching ${preset}`);
+      const res = await fetch(filterUrl, {
+        credentials: "include",
+        headers: {
+          Accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+          "Accept-Language": "en-GB,en;q=0.5",
+          Cookie: `POESESSID=${poesessid}`,
+          "Upgrade-Insecure-Requests": "1",
+          "Sec-Fetch-Dest": "document",
+          "Sec-Fetch-Mode": "navigate",
+          "Sec-Fetch-Site": "same-origin",
+          "Sec-Fetch-User": "?1",
+          Pragma: "no-cache",
+          "Cache-Control": "no-cache",
+        },
+        method: "GET",
+        mode: "cors",
+      });
 
       const resText = (await res.text())
         .replace(/^.*<pre.*class="item-filter-view-filter">/s, "")
@@ -92,12 +87,35 @@ export const fetchAndSaveFilters = async () => {
       if (resText.includes("!DOCTYPE")) {
         throw new Error("It looks like your POESESSID has expired");
       }
-      writeFileSync(resolve(".", "base-filters", "46custom.filter"), resText);
+      writeFileSync(
+        resolve(".", "base-filters", `${level}${filename}`),
+        resText
+      );
       console.info("NeverSink-5uberstr-softcore snippet", resText.slice(0, 32));
     } catch (e) {
-      console.error("Error when fetching NeverSink-5uberstr-softcore");
+      console.error(`Error when fetching ${preset}`);
       console.error("Your POESESSID may have expired.");
       console.error(e);
+      return 1;
+    }
+
+    return 0;
+  }
+};
+
+export const fetchAndSaveFilters = async () => {
+  console.info("Fetching filters");
+  for (const filter of FILTER_BLAST) {
+    for (const preset of filter.presets) {
+      console.info(`Fetching ${filter.group} ${preset.preset}`);
+      const fullUrl = `${filter.url}&preset=${preset.preset}`;
+      await fetchAndSaveFilter(fullUrl, `${preset.level}${preset.filename}`);
+    }
+  }
+
+  if (process.env.POESESSID) {
+    for (const filter of GGG_FILTERS) {
+      await fetchGggFilter(process.env.POESESSID, filter);
     }
   }
 
