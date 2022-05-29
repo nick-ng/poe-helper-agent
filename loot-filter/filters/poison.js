5f7d593dab91607d14ed5ff6947fc59f21afa35b
Nick Ng <nicholas.cj.ng@gmail.com>
2022-05-30 09:20:09 +1200

    added +1 phys spells recipe to poison

--- loot-filter/filters/poison.js
+++ loot-filter/filters/poison.js
@@ -70,6 +70,17 @@ Show
   MinimapIcon 1 Pink Star
   CustomAlertSound "sounds/brian-06-shing.mp3"
 
+Show  # +1 all Physical Gems recipe
+  Quality >= 1
+  Class "Gems"
+  BaseType "Sweep" "Cleave" "Shield Charge" "Added Fire Damage Support" "Melee Physical Damage Support" "Punishment" "Bloodlust Support" "Molten Shell" "Vaal Molten Shell" "Determination" "Iron Grip Support" "Shockwave Totem" "Holy Flame Totem" "Animate Guardian" "Herald of Purity" "Reckoning" "Vengeance" "Summon Stone Golem" "Chance to Bleed Support" "Maim Support" "Brutality Support" "Vulnerability" "War Banner" "Dread Banner" "Perforate" "Pride" "Awakened Added Fire Damage Support" "Awakened Brutality Support" "Awakened Melee Physical Damage Support" "Intimidating Cry" "Exsanguinate" "Corrupting Fever" "Bloodthirst Support" "Reap" "Defiance Banner" "Absolution" "Shield Crush" "Boneshatter" "Blade Flurry" "Double Strike" "Vaal Double Strike" "Lacerate" "Unearth" "Split Arrow" "Blood Rage" "Phase Run" "Puncture" "Bear Trap" "Ethereal Knives" "Cyclone" "Vaal Cyclone" "Spectral Shield Throw" "Animate Weapon" "Vicious Projectiles Support" "Herald of Agony" "Riposte" "Bladefall" "Blade Vortex" "Vaal Blade Vortex" "Explosive Trap" "Seismic Trap" "Withering Touch Support" "Lancing Steel" "Shattering Steel" "Impale Support" "Shrapnel Ballista" "Awakened Vicious Projectiles Support" "Blade Blast" "Splitting Steel" "Tornado" "Storm Burst" "Glacial Cascade" "Physical to Lightning Support" "Purifying Flame" "Wave of Conviction" "Divine Ire" "Summon Carrion Golem" "Penance Brand" "Void Sphere" "Hydrosphere" "Summon Reaper"
+  SetFontSize 45
+  SetTextColor 30 190 190 255
+  SetBorderColor 255 255 0 255
+  MinimapIcon 1 Yellow Triangle
+  PlayEffect Grey
+  CustomAlertSound "sounds/brian-01-clang.mp3"
+
 Hide
   AreaLevel > 3
   ItemLevel < 43
