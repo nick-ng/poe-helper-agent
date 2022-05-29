5ad6cef6555585f102b39ad630a8bd5cd8139028
Nick Ng <nicholas.cj.ng@gmail.com>
2022-05-29 20:31:19 +1200

    made doof sound shorter

--- loot-filter/filters.js
+++ loot-filter/filters.js
@@ -136,7 +136,16 @@ Show
   SetFontSize 45
   SetTextColor 20 240 240 255
   SetBorderColor 0 0 240 255
-  CustomAlertSound "sounds/brian-05-woosh.mp3"
+  CustomAlertSound "sounds/brian-01-clang.mp3"
+
+Show
+  Rarity Unique
+  SetFontSize 40
+	SetTextColor 175 96 37 255
+	SetBorderColor 175 96 37 255
+  CustomAlertSound "sounds/brian-03-bong.mp3"
+  PlayEffect Brown
+  MinimapIcon 2 Brown Star
 `;
 };
 
