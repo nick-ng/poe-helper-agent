d659b8bbdc558571c7c5726c7b0ada64a779a564
Nick Ng <nicholas.cj.ng@gmail.com>
2023-01-15 11:15:14 +1300

    Update - Sun, Jan 15, 2023 11:15:14 AM

--- loot-filter/filters/srs.js
+++ loot-filter/filters/srs.js
@@ -1,25 +1,6 @@
-import { levelingBaseFilter } from "../filters.js";
-import {
-  make3LinkFilter,
-  make4LinkFilter,
-  makeWeaponBlock,
-} from "../common/generators.js";
-import { getFilterFragment } from "../filter-loader.js";
-import getFlaskFilter, {
-  getManaFlaskFilter,
-} from "../../filter-generators/flasks.js";
-
-const amulets = '"Jade Amulet" "Lapis Amulet" "Turquoise Amulet"';
+import getTRLeveling from "./tr-leveling.js";
 
 const custom = `
-Show
-  AreaLevel < 20
-  BaseType ${amulets}
-  SetFontSize 45
-  PlayAlertSound 16 200
-  MinimapIcon 1 Green Moon
-  SetBorderColor 255 55 255 255
-
 Show
   Rarity Unique
   BaseType == "Legion Boots"
@@ -86,15 +67,6 @@ Show
   ##DefaultBackground
 
 Show
-  AreaLevel < 68
-  Rarity <= Rare
-  Corrupted False
-  BaseType "Short Bow" "Grove Bow" "Thicket Bow" "Reflex Bow" "Steelwood Bow" "Maraketh Bow"
-  SetFontSize 40
-  ##GoodBaseBorder
-  ##DefaultBackground
-
-Show
   Class "Currency"
   BaseType "of Fear"
   SetFontSize 45
@@ -104,15 +76,6 @@ Show
   PlayAlertSound 2 50
   PlayEffect White
   MinimapIcon 2 White Circle
-
-Show
-  AreaLevel < 65
-  BaseType == "Divine Mana Flask"
-  Rarity == Magic
-  SetBorderColor 80 150 255 200
-  ##DefaultBackground
-  MinimapIcon 2 Blue Raindrop
-  CustomAlertSound "sounds/ding.mp3"
 
 Show
   ItemLevel >= 50
@@ -139,62 +102,9 @@ Show
   MinimapIcon 0 Red Star
   PlayEffect Red
   CustomAlertSound "sounds/ding.mp3"
-
-Hide
-  AreaLevel > 3
-  ItemLevel < 43
-  Rarity = Normal
-  BaseType == "Rustic Sash" "Cloth Belt" "Studded Belt"
-
-Hide
-  AreaLevel > 3
-  ItemLevel < 43
-  Rarity = Normal
-  Class "Quivers" "One Hand" "Daggers" "Rune Dagger" "Staves" "Two Hand" "Bows" "Claws" "Warstaves"
 `;
-
-const weapons = [[12, "Longsword", false]];
 
 // https://textreader.pro/
 export default function getFilter() {
-  return [
-    custom,
-
-    ...weapons.map((weapon) => {
-      const [maxAreaLevel, baseType, sound] = weapon;
-      return makeWeaponBlock(maxAreaLevel, baseType, sound);
-    }),
-
-    make4LinkFilter("GGGR", "3g1r"),
-    make4LinkFilter("GGGG", "4g"),
-    // make4LinkFilter("BBBR", "3b1r"),
-
-    // 3-Links
-    make3LinkFilter("GGR", "Boots", "2g1r boots"),
-    make3LinkFilter("GGR", "Gloves", "2g1r gloves"),
-    make3LinkFilter("GGR", "Helmets", "2g1r helm"),
-    make3LinkFilter("GGR", "Body Armours", "2g1r body"),
-
-    make3LinkFilter("GGG", "Boots", "3g boots"),
-    make3LinkFilter("GGG", "Gloves", "3g gloves"),
-    make3LinkFilter("GGG", "Helmets", "3g helm"),
-    make3LinkFilter("GGG", "Body Armours", "3g body"),
-
-    // make3LinkFilter("BBB", "Boots", "3b boots"),
-    // make3LinkFilter("BBB", "Gloves", "3b gloves"),
-    // make3LinkFilter("BBB", "Helmets", "3b helm"),
-    // make3LinkFilter("BBB", "Body Armours", "3b body"),
-    // make3LinkFilter("BBB", "Sceptre", "3b sceptre"),
-
-    // make3LinkFilter("BBR", "Boots", "2b1r", 1, 62),
-    // make3LinkFilter("BBR", "Gloves", "2b1r", 1, 62),
-    // make3LinkFilter("BBR", "Helmets", "2b1r", 1, 62),
-    // make3LinkFilter("BBR", "Body Armours", "2b1r", 1, 62),
-    // make3LinkFilter("BBR", "Sceptre", "2b1r", 1, 62),
-
-    levelingBaseFilter(),
-    getFilterFragment("ssf-bases", { amulets }),
-    getFlaskFilter(),
-    getManaFlaskFilter(),
-  ].join("\n\n");
+  return [custom, getTRLeveling()].join("\n\n");
 }
--- loot-filter/filters/tr-leveling.js
+++ loot-filter/filters/tr-leveling.js
@@ -1,110 +1,120 @@
 import { levelingBaseFilter } from "../filters.js";
 import {
   make3LinkFilter,
-  make3LinkFilterB,
   make4LinkFilter,
+  makeWeaponBlock,
 } from "../common/generators.js";
+import { getFilterFragment } from "../filter-loader.js";
+import getFlaskFilter, {
+  getManaFlaskFilter,
+} from "../../filter-generators/flasks.js";
 
-const bows = [
-  "Short Bow",
-  "Royal Bow",
-  "Reflex Bow",
-  "Grove Bow",
-  "Highborn Bow",
-  "Steelwood Bow",
-  "Imperial Bow",
-  "Thicket Bow",
-  "Maraketh Bow",
-];
+const amulets = '"Jade Amulet" "Lapis Amulet" "Turquoise Amulet"';
 
-const custom = `Show
-  SetBorderColor 200 0 0
-  SetFontSize 45
-  ItemLevel >= 1
-  BaseType "Rustic Sash"
-  ItemLevel <= 30
-  CustomAlertSound "sounds/rustic sash.mp3"
-
+const custom = `
 Show
-  BaseType "Long Bow" "Crude Bow" "Short Bow" "Imperial Bow"
-  Rarity Unique
+  AreaLevel < 20
+  BaseType ${amulets}
   SetFontSize 45
-  SetBackgroundColor 255 0 255 255
-  SetBorderColor 255 255 255 255
   PlayAlertSound 16 200
-  MinimapIcon 1 Pink Star
+  MinimapIcon 1 Green Moon
+  SetBorderColor 255 55 255 255
 
 Show
-  BaseType "Long Bow" "Crude Bow" "Short Bow" "Imperial Bow"
-  Rarity Unique
-  SetFontSize 35
-  SetBackgroundColor 255 0 255 255
-  SetBorderColor 214 126 0 255
-  MinimapIcon 1 Pink Cross
-
-Show
-  AreaLevel <= 75
-  BaseType == "${bows.join('" "')}"
-  ItemLevel >= 25
+  Rarity = Unique
+  Class "Rings"
   SetFontSize 45
+  SetTextColor 255 255 255 255
+  SetBackgroundColor 255 0 0 255
+  SetBorderColor 100 100 100 255
+  PlayEffect Red
+  MinimapIcon 2 Brown Star
+
+Show
+  AreaLevel <= 77
+  ItemLevel >= 64
   Rarity <= Rare
-  SetBackgroundColor 255 0 255 255
-  SetBorderColor 0 0 0 255
-  MinimapIcon 1 Pink Cross
-  PlayAlertSound 16 200
+  Corrupted False
+  BaseType "Short Bow" "Grove Bow" "Thicket Bow" "Reflex Bow" "Steelwood Bow" "Maraketh Bow"
+  SetFontSize 45
+  PlayEffect Blue
+  ##GoodBaseBorder
+  ##DefaultBackground
+
+Show
+  AreaLevel < 68
+  ItemLevel >= 9
+  Rarity <= Rare
+  Corrupted False
+  BaseType "Short Bow" "Grove Bow" "Thicket Bow" "Reflex Bow" "Steelwood Bow" "Maraketh Bow"
+  SetFontSize 40
+  ##GoodBaseBorder
+  ##DefaultBackground
+
+Show
+  AreaLevel < 65
+  BaseType == "Divine Mana Flask"
+  Rarity == Magic
+  SetBorderColor 80 150 255 200
+  ##DefaultBackground
+  MinimapIcon 2 Blue Raindrop
+  CustomAlertSound "sounds/ding.mp3"
+
+Hide
+  AreaLevel > 3
+  ItemLevel < 43
+  Rarity = Normal
+  BaseType == "Rustic Sash" "Cloth Belt" "Studded Belt"
+
+Hide
+  AreaLevel > 3
+  ItemLevel < 43
+  Rarity = Normal
+  Class "Quivers" "One Hand" "Daggers" "Rune Dagger" "Staves" "Two Hand" "Bows" "Claws" "Warstaves"
 `;
 
-const weapons = [
-  [12, "Longsword", true],
-  [18, "Woodsplitter", true],
-  [18, "Bastard Sword", true],
-  [23, "Poleaxe", true],
-  [28, "Double Axe", true],
-  [33, "Gilded Axe", true],
-  [37, "Shadow Axe", false],
-  // [41, "Jasper Chopper", true],
-  // [41, "Dagger Axe", true],
-  // [45, "Timber Axe", true],
-  // [49, "Headsman Axe", true],
-  // [52, "Labrys", true],
-  // [55, "Noble Axe", true],
-  // [59, "Abyssal Axe", false],
-];
+const weapons = [[12, "Longsword", false]];
 
 // https://textreader.pro/
 export default function getFilter() {
-  return "";
-
   return [
     custom,
+
     ...weapons.map((weapon) => {
       const [maxAreaLevel, baseType, sound] = weapon;
       return makeWeaponBlock(maxAreaLevel, baseType, sound);
     }),
 
-    // 4-Links
-    make4LinkFilter("GGGR", "3g1r", 1, 62),
-    make4LinkFilter("GGGG", "4g", 25, 62),
+    make4LinkFilter("GGGR", "3g1r"),
+    make4LinkFilter("GGGG", "4g"),
+    // make4LinkFilter("BBBR", "3b1r"),
 
     // 3-Links
     make3LinkFilter("GGR", "Boots", "2g1r boots"),
     make3LinkFilter("GGR", "Gloves", "2g1r gloves"),
     make3LinkFilter("GGR", "Helmets", "2g1r helm"),
     make3LinkFilter("GGR", "Body Armours", "2g1r body"),
+
     make3LinkFilter("GGG", "Boots", "3g boots"),
     make3LinkFilter("GGG", "Gloves", "3g gloves"),
     make3LinkFilter("GGG", "Helmets", "3g helm"),
     make3LinkFilter("GGG", "Body Armours", "3g body"),
 
-    // Other People
-    make3LinkFilter("BBB", "Wand", "ding"),
-    make3LinkFilterB("BBB", "ding"),
-    make3LinkFilter("BBG", "Wand", "ding"),
-    make3LinkFilterB("BBG", "ding"),
-    make3LinkFilter("BGG", "Wand", "ding"),
-    make3LinkFilterB("BGG", "ding"),
+    // make3LinkFilter("BBB", "Boots", "3b boots"),
+    // make3LinkFilter("BBB", "Gloves", "3b gloves"),
+    // make3LinkFilter("BBB", "Helmets", "3b helm"),
+    // make3LinkFilter("BBB", "Body Armours", "3b body"),
+    // make3LinkFilter("BBB", "Sceptre", "3b sceptre"),
 
-    // other stuff
+    // make3LinkFilter("BBR", "Boots", "2b1r", 1, 62),
+    // make3LinkFilter("BBR", "Gloves", "2b1r", 1, 62),
+    // make3LinkFilter("BBR", "Helmets", "2b1r", 1, 62),
+    // make3LinkFilter("BBR", "Body Armours", "2b1r", 1, 62),
+    // make3LinkFilter("BBR", "Sceptre", "2b1r", 1, 62),
+
     levelingBaseFilter(),
+    getFilterFragment("ssf-bases", { amulets }),
+    getFlaskFilter(),
+    getManaFlaskFilter(),
   ].join("\n\n");
 }
