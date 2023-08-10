aa1fc1cde981010f8a5f90e06338ac90c25893ac
Nick Ng <nick-ng@users.noreply.github.com>
2023-08-10 22:30:02 +1200

    Update - 10 Aug 2023 22:30:02

--- loot-filter/filters/phase-1.js
+++ loot-filter/filters/phase-1.js
@@ -2,6 +2,16 @@ import get2hMeleeFilter from "./2h-melee.js";
 import getPhase2 from "./phase-2.js";
 
 const custom = `
+Show
+  Rarity Unique
+  BaseType == "Carved Wand" "Ezomyte Dagger" "Soldier Helmet"
+  SetFontSize 45
+  ##BrightBackground
+  SetTextColor 255 140 54 255
+  SetBorderColor 47 252 47 255
+  MinimapIcon 2 Brown Star
+  PlayEffect Brown
+  CustomAlertSound "sounds/brian-03-bong.mp3"
 `;
 
 export default function getFilter() {
