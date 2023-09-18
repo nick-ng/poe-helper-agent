9e0d540c52b299d95e3832934cdec7bb2ed486d5
Nick Ng <nick-ng@users.noreply.github.com>
2023-09-18 20:51:25 +1200

    feat: depend on base filter for uniques

--- loot-filter/filters.js
+++ loot-filter/filters.js
@@ -1,35 +1,35 @@
 import { getFilterFragment } from "./filter-loader.js";
 import {
-  make3LinkFilter,
-  make4LinkFilter,
-  makeBaseTypeFilter,
+	make3LinkFilter,
+	make4LinkFilter,
+	makeBaseTypeFilter,
 } from "./common/generators.js";
 
 export const uniqueDivinationCards = makeBaseTypeFilter(
-  [
-    "The Coming Storm", // Lightning Coil
-    "Dark Temptation", // Obliteration
-    "A Dab of Ink", // The Poet's Pen
-    "A Fate Worse Than Death", // Cortex
-    "Broken Truce", // Cold Iron Point
-    "Lethean Temptation", // Nebulis
-  ],
-  [
-    "SetFontSize 45",
-    "SetTextColor 175 96 37 255",
-    "SetBorderColor 47 252 47 255",
-    "MinimapIcon 2 Green Triangle",
-    "PlayEffect Brown",
-    'CustomAlertSound "sounds/brian-03-bong.mp3"',
-  ]
+	[
+		"The Coming Storm", // Lightning Coil
+		"Dark Temptation", // Obliteration
+		"A Dab of Ink", // The Poet's Pen
+		"A Fate Worse Than Death", // Cortex
+		"Broken Truce", // Cold Iron Point
+		"Lethean Temptation", // Nebulis
+	],
+	[
+		"SetFontSize 45",
+		"SetTextColor 175 96 37 255",
+		"SetBorderColor 47 252 47 255",
+		"MinimapIcon 2 Green Triangle",
+		"PlayEffect Brown",
+		'CustomAlertSound "sounds/brian-03-bong.mp3"',
+	]
 );
 
 export const baseFilter = () =>
-  [
-    getFilterFragment("ssf-bases-top", {}),
-    getFilterFragment("base", {}),
-    uniqueDivinationCards,
-    `Show
+	[
+		getFilterFragment("ssf-bases-top", {}),
+		getFilterFragment("base", {}),
+		uniqueDivinationCards,
+		`Show
   GemLevel > 10
   Class "Gems"
   SetFontSize 45
@@ -37,39 +37,10 @@ export const baseFilter = () =>
   SetBorderColor 0 0 240 255
   CustomAlertSound "sounds/brian-01-clang.mp3"
 `,
-  ].join("\n\n");
+	].join("\n\n");
 
 export const levelingBaseFilter = () =>
-  [getFilterFragment("leveling"), baseFilter()].join("\n\n");
-
-export const topUniquesFilter = `
-# Top Uniques
-Show
-  BaseType == "Champion Kite Shield" "Ghastly Eye Jewel" "Silk Gloves" "Blunt Arrow Quiver Piece" "Legion Sword Piece" "Hypnotic Eye Jewel" "Carnal Sceptre" "Archon Kite Shield" "Timeless Jewel" "Runic Crown" "Small Cluster Jewel" "Steel Ring" "Ezomyte Dagger" "Carnal Boots" "Jewelled Foil" "Fluted Bascinet" "Crusader Boots" "Sadist Garb" "Saint's Hauberk" "Prismatic Jewel" "Greatwolf Talisman" "Harlequin Mask" "Assassin's Boots" "Imperial Staff Piece" "Archon Kite Shield Piece" "Callous Mask Piece" "Cloth Belt Piece" "Stealth Boots" "Arcanist Gloves" "Wyrmscale Doublet" "Zodiac Leather" "Glorious Plate" "Exquisite Leather" "Jingling Spirit Shield" "Granite Flask" "Pinnacle Tower Shield" "Ornate Quiver" "Medium Cluster Jewel" "Vaal Rapier" "Blood Raiment" "Siege Axe" "Rawhide Boots" "Occultist's Vestment" "Sapphire Flask" "Ezomyte Tower Shield" "Gladiator Plate" "Royal Burgonet" "Imperial Maul" "Unset Amulet" "Large Cluster Jewel" "Prophecy Wand" "Citadel Bow"
-  Rarity = Unique
-  AreaLevel > 69
-  SetFontSize 45
-  SetTextColor 255 0 0 255
-  SetBackgroundColor 255 255 255 255
-  SetBorderColor 255 0 0 255
-  MinimapIcon 0 Red Star
-  PlayEffect Red
-  PlayAlertSound 6 300
-
-# Unsure Uniques
-Show
-  BaseType == "Goathide Boots" "Ezomyte Burgonet" "Sinner Tricorne" "Cobalt Jewel" "Crimson Jewel" "Vaal Spirit Shield" "Fiend Dagger" "Assassin Bow" "Two-Point Arrow Quiver" "Onyx Amulet" "Paua Amulet" "Judgement Staff" "Turquoise Amulet" "Leather Belt" "Heavy Belt" "Eternal Sword" "Gavel" "Viridian Jewel" "Varnished Coat" "Ebony Tower Shield" "Prophet Crown" "Citrine Amulet" "Nightmare Bascinet" "Imperial Skean" "Sorcerer Boots" "Iron Flask" "Agate Amulet" "Necromancer Silks" "Jade Amulet" "Highborn Staff" "Sage Wand" "Studded Belt" "Stibnite Flask" "Amber Amulet" "Elegant Round Shield"
-  Rarity = Unique
-  AreaLevel > 69
-  SetFontSize 45
-  LinkedSockets < 5
-  SetTextColor 255 0 0 255
-  SetBackgroundColor 200 200 200 255
-  SetBorderColor 255 0 0 255
-  MinimapIcon 0 Red Cross
-  PlayEffect Red
-  PlayAlertSound 2 300
-`;
+	[getFilterFragment("leveling"), baseFilter()].join("\n\n");
 
 export const oniGoroshiFarm = `
 Show
@@ -126,10 +97,10 @@ export const otherHider = `
  * @param {number} size
  */
 export const makeBodyFilter = (size) => {
-  if (size <= 0) {
-    return `# Body: ${Math.ceil(size)}`;
-  }
-  return `Show
+	if (size <= 0) {
+		return `# Body: ${Math.ceil(size)}`;
+	}
+	return `Show
     Class "Body Armour"
     SetBorderColor 255 255 255
     AreaLevel <= 68
@@ -148,10 +119,10 @@ export const makeBodyFilter = (size) => {
  * @param {number} size
  */
 export const makeGlovesFilter = (size) => {
-  if (size <= 0) {
-    return `# Gloves: ${Math.ceil(size)}`;
-  }
-  return `Show
+	if (size <= 0) {
+		return `# Gloves: ${Math.ceil(size)}`;
+	}
+	return `Show
     Class "Gloves"
     SetBorderColor 255 0 0
     AreaLevel <= 68
@@ -168,10 +139,10 @@ export const makeGlovesFilter = (size) => {
  * @param {number} size
  */
 export const makeBootsFilter = (size) => {
-  if (size <= 0) {
-    return `# Boots: ${Math.ceil(size)}`;
-  }
-  return `Show
+	if (size <= 0) {
+		return `# Boots: ${Math.ceil(size)}`;
+	}
+	return `Show
     Class "Boots"
     SetBorderColor 0 0 255
     AreaLevel <= 68
@@ -188,10 +159,10 @@ export const makeBootsFilter = (size) => {
  * @param {number} size
  */
 export const makeHelmFilter = (size) => {
-  if (size <= 0) {
-    return `# Helm: ${Math.ceil(size)}`;
-  }
-  return ` Show
+	if (size <= 0) {
+		return `# Helm: ${Math.ceil(size)}`;
+	}
+	return ` Show
     Class "Helm"
     SetBorderColor 0 255 0
     AreaLevel <= 68
@@ -208,10 +179,10 @@ export const makeHelmFilter = (size) => {
  * @param {number} size
  */
 export const makeRingFilter = (size) => {
-  if (size <= 0) {
-    return `# Ring: ${Math.ceil(size)}`;
-  }
-  return `Show
+	if (size <= 0) {
+		return `# Ring: ${Math.ceil(size)}`;
+	}
+	return `Show
     Class "Ring"
     SetBorderColor 255 0 255
     AreaLevel <= 68
@@ -228,10 +199,10 @@ export const makeRingFilter = (size) => {
  * @param {number} size
  */
 export const makeAmuletFilter = (size) => {
-  if (size <= 0) {
-    return `# Amulet: ${Math.ceil(size)}`;
-  }
-  return `Show
+	if (size <= 0) {
+		return `# Amulet: ${Math.ceil(size)}`;
+	}
+	return `Show
     Class "Amulet"
     SetBorderColor 255 255 0
     AreaLevel <= 68
@@ -248,10 +219,10 @@ export const makeAmuletFilter = (size) => {
  * @param {number} size
  */
 export const makeBeltFilter = (size) => {
-  if (size <= 0) {
-    return `# Belt: ${Math.ceil(size)}`;
-  }
-  return `Show
+	if (size <= 0) {
+		return `# Belt: ${Math.ceil(size)}`;
+	}
+	return `Show
     Class "Belt"
     SetBorderColor 0 255 255
     AreaLevel <= 68
@@ -268,10 +239,10 @@ export const makeBeltFilter = (size) => {
  * @param {number} size
  */
 export const makeWeaponFilter = (size) => {
-  if (size <= 0) {
-    return `# Weapon: ${Math.ceil(size)}`;
-  }
-  return `Show
+	if (size <= 0) {
+		return `# Weapon: ${Math.ceil(size)}`;
+	}
+	return `Show
     AreaLevel <= 68
     ItemLevel >= 60
     ItemLevel <= 70
@@ -302,20 +273,20 @@ Show
 };
 
 export const makeAllLinkFilters = () => {
-  return [
-    // 4-Links
-    // Aura Bot
-    make4LinkFilter("RRRB", "3r1b"),
-    make4LinkFilter("RRBB", "2b2r"),
-    // Caster, Toxic Rain
-    make4LinkFilter("BBBG", "3b1g"),
-    make4LinkFilter("GGGB", "3g1b"),
-    make4LinkFilter("GGGG", "4g"),
-    make4LinkFilter("BBBB", "4b"),
-    make4LinkFilter("RRRG", "ding"),
-    make4LinkFilter("RRRR", "ding"),
-    make4LinkFilter("RRGG", "ding"),
-    `
+	return [
+		// 4-Links
+		// Aura Bot
+		make4LinkFilter("RRRB", "3r1b"),
+		make4LinkFilter("RRBB", "2b2r"),
+		// Caster, Toxic Rain
+		make4LinkFilter("BBBG", "3b1g"),
+		make4LinkFilter("GGGB", "3g1b"),
+		make4LinkFilter("GGGG", "4g"),
+		make4LinkFilter("BBBB", "4b"),
+		make4LinkFilter("RRRG", "ding"),
+		make4LinkFilter("RRRR", "ding"),
+		make4LinkFilter("RRGG", "ding"),
+		`
 Show
   AreaLevel < 40
   Sockets < 6
@@ -329,39 +300,39 @@ Show
   PlayAlertSound 2 70
 `,
 
-    // 3-Links
-    // Caster
-    make3LinkFilter("BBB", "Wand", "3b wand"),
-    make3LinkFilter("BBB", "Boots", "3b boots"),
-    make3LinkFilter("BBB", "Gloves", "3b gloves"),
-    make3LinkFilter("BBB", "Helmets", "3b helm"),
-    make3LinkFilter("BBB", "Body Armours", "3b body"),
-    make3LinkFilter("BBG", "Wand", "2b1g wand"),
-    make3LinkFilter("BBG", "Boots", "2b1g boots"),
-    make3LinkFilter("BBG", "Gloves", "2b1g gloves"),
-    make3LinkFilter("BBG", "Helmets", "2b1g helm"),
-    make3LinkFilter("BBG", "Body Armours", "2b1g body"),
-    make3LinkFilter("BGG", "Boots", "2g1b boots"),
-    make3LinkFilter("BGG", "Gloves", "2g1b gloves"),
-    make3LinkFilter("BGG", "Helmets", "2g1b helm"),
-    make3LinkFilter("BGG", "Body Armours", "2g1b body"),
-    make3LinkFilter("GGG", "Boots", "3g boots"),
-    make3LinkFilter("GGG", "Gloves", "3g gloves"),
-    make3LinkFilter("GGG", "Helmets", "3g helm"),
-    make3LinkFilter("GGG", "Body Armours", "3g body"),
-    make3LinkFilter("GGR", "Boots", "2g1r"),
-    make3LinkFilter("GGR", "Gloves", "2g1r"),
-    make3LinkFilter("GGR", "Helmets", "2g1r"),
-    make3LinkFilter("GGR", "Body Armours", "2g1r"),
-    make3LinkFilter("RRR", "Boots", "ding"),
-    make3LinkFilter("RRR", "Gloves", "ding"),
-    make3LinkFilter("RRR", "Helmets", "ding"),
-    make3LinkFilter("RRR", "Body Armours", "ding"),
-    make3LinkFilter("RRG", "Boots", "ding"),
-    make3LinkFilter("RRG", "Gloves", "ding"),
-    make3LinkFilter("RRG", "Helmets", "ding"),
-    make3LinkFilter("RRG", "Body Armours", "ding"),
-  ].join("\n");
+		// 3-Links
+		// Caster
+		make3LinkFilter("BBB", "Wand", "3b wand"),
+		make3LinkFilter("BBB", "Boots", "3b boots"),
+		make3LinkFilter("BBB", "Gloves", "3b gloves"),
+		make3LinkFilter("BBB", "Helmets", "3b helm"),
+		make3LinkFilter("BBB", "Body Armours", "3b body"),
+		make3LinkFilter("BBG", "Wand", "2b1g wand"),
+		make3LinkFilter("BBG", "Boots", "2b1g boots"),
+		make3LinkFilter("BBG", "Gloves", "2b1g gloves"),
+		make3LinkFilter("BBG", "Helmets", "2b1g helm"),
+		make3LinkFilter("BBG", "Body Armours", "2b1g body"),
+		make3LinkFilter("BGG", "Boots", "2g1b boots"),
+		make3LinkFilter("BGG", "Gloves", "2g1b gloves"),
+		make3LinkFilter("BGG", "Helmets", "2g1b helm"),
+		make3LinkFilter("BGG", "Body Armours", "2g1b body"),
+		make3LinkFilter("GGG", "Boots", "3g boots"),
+		make3LinkFilter("GGG", "Gloves", "3g gloves"),
+		make3LinkFilter("GGG", "Helmets", "3g helm"),
+		make3LinkFilter("GGG", "Body Armours", "3g body"),
+		make3LinkFilter("GGR", "Boots", "2g1r"),
+		make3LinkFilter("GGR", "Gloves", "2g1r"),
+		make3LinkFilter("GGR", "Helmets", "2g1r"),
+		make3LinkFilter("GGR", "Body Armours", "2g1r"),
+		make3LinkFilter("RRR", "Boots", "ding"),
+		make3LinkFilter("RRR", "Gloves", "ding"),
+		make3LinkFilter("RRR", "Helmets", "ding"),
+		make3LinkFilter("RRR", "Body Armours", "ding"),
+		make3LinkFilter("RRG", "Boots", "ding"),
+		make3LinkFilter("RRG", "Gloves", "ding"),
+		make3LinkFilter("RRG", "Helmets", "ding"),
+		make3LinkFilter("RRG", "Body Armours", "ding"),
+	].join("\n");
 };
 
 export const rusticSash = `Show
@@ -373,9 +344,9 @@ export const rusticSash = `Show
   CustomAlertSound "sounds/rustic sash.mp3"`;
 
 export const makeLevelingFilter = () => {
-  return `
-${topUniquesFilter}
+	return `
 
 ${levelingBaseFilter()}
+
 `;
 };
--- loot-filter/index.js
+++ loot-filter/index.js
@@ -5,122 +5,121 @@ import { oniGoroshiFarm, baseFilter } from "./filters.js";
 import { getCustomItemsFilter } from "./filter-loader.js";
 import { getChaosFilter } from "./chaos-filter.js";
 import { writeFilters } from "./write-filters.js";
-import { getSsfUniquesFilter } from "../filter-generators/uniques.js";
 
 export const makeChaosFilter = (
-  itemCounts,
-  outputDir,
-  isDebug = false,
-  chaosOnly = false
+	itemCounts,
+	outputDir,
+	isDebug = false,
+	chaosOnly = false
 ) => {
-  const chaosFilter = getChaosFilter(itemCounts, chaosOnly);
-  writeFilters(
-    [chaosFilter],
-    outputDir,
-    { prefix: "99_chaos", suffix: "" },
-    isDebug,
-    [5, 10]
-  );
+	const chaosFilter = getChaosFilter(itemCounts, chaosOnly);
+	writeFilters(
+		[chaosFilter],
+		outputDir,
+		{ prefix: "99_chaos", suffix: "" },
+		isDebug,
+		[5, 10]
+	);
 };
 
 export const autoMakeFilters = (outputDir, isDebug = false) => {
-  writeFilters(
-    [getCustomItemsFilter(), baseFilter(), getSsfUniquesFilter()],
-    outputDir,
-    { prefix: "1_t16-maps_", suffix: "" },
-    isDebug,
-    [19, 25]
-  );
-  writeFilters(
-    [getCustomItemsFilter(), baseFilter(), getSsfUniquesFilter()],
-    outputDir,
-    { prefix: "2_t16-maps_", suffix: "" },
-    isDebug,
-    [26, 999]
-  );
+	writeFilters(
+		[getCustomItemsFilter(), baseFilter()],
+		outputDir,
+		{ prefix: "1_t16-maps_", suffix: "" },
+		isDebug,
+		[19, 25]
+	);
+	writeFilters(
+		[getCustomItemsFilter(), baseFilter()],
+		outputDir,
+		{ prefix: "2_t16-maps_", suffix: "" },
+		isDebug,
+		[26, 999]
+	);
 
-  writeFilters(
-    [oniGoroshiFarm],
-    outputDir,
-    { prefix: "yy_oni_farm_", suffix: "" },
-    isDebug,
-    [0, 2]
-  );
+	writeFilters(
+		[oniGoroshiFarm],
+		outputDir,
+		{ prefix: "yy_oni_farm_", suffix: "" },
+		isDebug,
+		[0, 2]
+	);
 
-  const moduleNames = readdirSync(resolve(".", "loot-filter", "filters"));
+	const moduleNames = readdirSync(resolve(".", "loot-filter", "filters"));
 
-  moduleNames.forEach(async (moduleName) => {
-    const { default: getFilter } = await import(`./filters/${moduleName}`);
+	moduleNames.forEach(async (moduleName) => {
+		const { default: getFilter } = await import(`./filters/${moduleName}`);
 
-    const filterName = moduleName.replace(".js", "");
+		const filterName = moduleName.replace(".js", "");
 
-    const filterText = getFilter();
-    if (!filterText) {
-      console.info(`Skipping ${filterName}`);
-      return;
-    }
+		const filterText = getFilter();
+		if (!filterText) {
+			console.info(`Skipping ${filterName}`);
+			return;
+		}
 
-    if (filterName.includes("maps")) {
-      console.info(`Writing ${filterName}`);
-      writeFilters(
-        [getCustomItemsFilter(), filterText, getSsfUniquesFilter()],
-        outputDir,
-        { prefix: `2_${filterName}_`, suffix: "" },
-        isDebug,
-        [19, 25]
-      );
-      writeFilters(
-        [getCustomItemsFilter(), filterText, getSsfUniquesFilter()],
-        outputDir,
-        { prefix: `3_${filterName}_`, suffix: "" },
-        isDebug,
-        [26, 999]
-      );
-    } else if (filterName.includes("phase")) {
-      console.info(`Writing ${filterName}`);
-      // league start
-      let filterRange = [0, 999];
+		if (filterName.includes("maps")) {
+			console.info(`Writing ${filterName}`);
+			writeFilters(
+				[getCustomItemsFilter(), filterText],
+				outputDir,
+				{ prefix: `2_${filterName}_`, suffix: "" },
+				isDebug,
+				[19, 25]
+			);
+			writeFilters(
+				[getCustomItemsFilter(), filterText],
+				outputDir,
+				{ prefix: `3_${filterName}_`, suffix: "" },
+				isDebug,
+				[26, 999]
+			);
+		} else if (filterName.includes("phase")) {
+			console.info(`Writing ${filterName}`);
+			// league start
+			let filterRange = [0, 999];
 
-      switch (filterName) {
-        case "phase-1": {
-          filterRange = [5, 11];
-          break;
-        }
-        case "phase-2": {
-          filterRange = [19, 25];
-          break;
-        }
-        default: {
-          filterRange = [26, 999];
-        }
-      }
+			switch (filterName) {
+				case "phase-1": {
+					filterRange = [5, 11];
+					break;
+				}
+				case "phase-2": {
+					filterRange = [19, 25];
+					break;
+				}
+				default: {
+					filterRange = [26, 999];
+				}
+			}
 
-      writeFilters(
-        [getCustomItemsFilter(), filterText, getSsfUniquesFilter()],
-        outputDir,
-        { prefix: `0_${filterName}_`, suffix: "" },
-        isDebug,
-        filterRange
-      );
-    } else if (filterName.includes("act-5")) {
-      // act 5 any %
-      writeFilters(
-        [filterText],
-        outputDir,
-        { prefix: `9_${filterName}_`, suffix: "" },
-        isDebug,
-        [5, 11]
-      );
-    } else {
-      console.info(`Skipping ${filterName}`);
-      return;
-      writeFilters(
-        [getCustomItemsFilter(), filterText, getSsfUniquesFilter()],
-        outputDir,
-        { prefix: `0_${filterName}_`, suffix: "" },
-        isDebug,
-        [5, 11]
-      );
-    }
-  });
+			writeFilters(
+				[getCustomItemsFilter(), filterText],
+				outputDir,
+				{ prefix: `0_${filterName}_`, suffix: "" },
+				isDebug,
+				filterRange
+			);
+		} else if (filterName.includes("act-5")) {
+			// act 5 any %
+			writeFilters(
+				[filterText],
+				outputDir,
+				{ prefix: `9_${filterName}_`, suffix: "" },
+				isDebug,
+				[5, 11]
+			);
+		} else {
+			console.info(`Skipping ${filterName}`);
+			return;
+			writeFilters(
+				[getCustomItemsFilter(), filterText],
+				outputDir,
+				{ prefix: `0_${filterName}_`, suffix: "" },
+				isDebug,
+				[5, 11]
+			);
+		}
+	});
 };
