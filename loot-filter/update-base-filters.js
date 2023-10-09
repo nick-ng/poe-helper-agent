81678952854bde9ba3f30401946f0fb9773ac162
Nick Ng <nick-ng@users.noreply.github.com>
2023-10-09 21:29:39 +1300

    feat: update how custom base filters are specified

--- loot-filter/update-base-filters.js
+++ loot-filter/update-base-filters.js
@@ -14,187 +14,180 @@ import { writeFileSync } from "./write-filters.js";
  * 50: nem 3
  */
 const FILTER_BLAST = [
-  {
-    group: "neversink",
-    url: "https://filterblast.xyz/api/FilterFile/?filter=NeverSink&colortheme=Normal&soundtheme=Default",
-    presets: [
-      { filename: "nsss.filter", preset: "SEMI-STRICT", level: 10 },
-      { filename: "nsvs.filter", preset: "VERY-STRICT", level: 20 },
-      { filename: "nsub.filter", preset: "UBER-STRICT", level: 30 },
-      // { filename: "nsup.filter", preset: "UBER-PLUS-STRICT", level: 50 },
-    ],
-  },
-  {
-    group: "StupidFatHobbit",
-    url: "https://filterblast.xyz/api/FilterFile/?filter=StupidFatHobbit",
-    presets: [
-      { filename: "sfh1.filter", preset: "General", level: 10 },
-      { filename: "sfh2.filter", preset: "Strict", level: 20 },
-      { filename: "shf3.filter", preset: "Uberstrict", level: 30 },
-    ],
-  },
-  {
-    group: "default",
-    url: "https://filterblast.xyz/api/FilterFile/?filter=Default",
-    presets: [{ filename: "default.filter", preset: "", level: 1 }],
-  },
+	{
+		group: "neversink",
+		url: "https://filterblast.xyz/api/FilterFile/?filter=NeverSink&colortheme=Normal&soundtheme=Default",
+		presets: [
+			{ filename: "nsss.filter", preset: "SEMI-STRICT", level: 10 },
+			{ filename: "nsvs.filter", preset: "VERY-STRICT", level: 20 },
+			{ filename: "nsub.filter", preset: "UBER-STRICT", level: 30 },
+			// { filename: "nsup.filter", preset: "UBER-PLUS-STRICT", level: 50 },
+		],
+	},
+	{
+		group: "StupidFatHobbit",
+		url: "https://filterblast.xyz/api/FilterFile/?filter=StupidFatHobbit",
+		presets: [
+			{ filename: "sfh1.filter", preset: "General", level: 10 },
+			{ filename: "sfh2.filter", preset: "Strict", level: 20 },
+			{ filename: "shf3.filter", preset: "Uberstrict", level: 30 },
+		],
+	},
+	{
+		group: "default",
+		url: "https://filterblast.xyz/api/FilterFile/?filter=Default",
+		presets: [{ filename: "default.filter", preset: "", level: 1 }],
+	},
 ];
 
 const GGG_FILTERS = [
-  // {
-  //   filename: "nsvs-l.filter",
-  //   preset: "neversink-very-strict-sc-live",
-  //   filterUrl: "https://www.pathofexile.com/item-filter/GmXoTD",
-  //   level: 19,
-  // },
-  // {
-  //   filename: "nsub-l.filter",
-  //   preset: "neversink-uber-strict-sc-live",
-  //   filterUrl: "https://www.pathofexile.com/item-filter/wvPeTJ",
-  //   level: 44,
-  // },
-  // {
-  // 	filename: "sfh1.filter",
-  // 	preset: "stupidfathobbit-general",
-  // 	filterUrl: "https://www.pathofexile.com/item-filter/A3BxSM",
-  // 	level: 10,
-  // },
-  // {
-  // 	filename: "sfh2.filter",
-  // 	preset: "stupidfathobbit-strict",
-  // 	filterUrl: "https://www.pathofexile.com/item-filter/zOv6Hz",
-  // 	level: 20,
-  // },
-  // {
-  // 	filename: "sfh3.filter",
-  // 	preset: "stupidfathobbit-uberstrict",
-  // 	filterUrl: "https://www.pathofexile.com/item-filter/Mpxoi0",
-  // 	level: 30,
-  // },
+	// {
+	//   filename: "nsvs-l.filter",
+	//   preset: "neversink-very-strict-sc-live",
+	//   filterUrl: "https://www.pathofexile.com/item-filter/GmXoTD",
+	//   level: 19,
+	// },
+	// {
+	//   filename: "nsub-l.filter",
+	//   preset: "neversink-uber-strict-sc-live",
+	//   filterUrl: "https://www.pathofexile.com/item-filter/wvPeTJ",
+	//   level: 44,
+	// },
+	// {
+	// 	filename: "sfh1.filter",
+	// 	preset: "stupidfathobbit-general",
+	// 	filterUrl: "https://www.pathofexile.com/item-filter/A3BxSM",
+	// 	level: 10,
+	// },
+	// {
+	// 	filename: "sfh2.filter",
+	// 	preset: "stupidfathobbit-strict",
+	// 	filterUrl: "https://www.pathofexile.com/item-filter/zOv6Hz",
+	// 	level: 20,
+	// },
+	// {
+	// 	filename: "sfh3.filter",
+	// 	preset: "stupidfathobbit-uberstrict",
+	// 	filterUrl: "https://www.pathofexile.com/item-filter/Mpxoi0",
+	// 	level: 30,
+	// },
 ];
 
 export const presets = _.flatMap(
-  [
-    // ...FILTER_BLAST, // RIP filterblast.xyz ?
-    // { presets: GGG_FILTERS },
-    {
-      presets: [
-        { filename: "aaa.filter", preset: "SEMI-STRICT", level: 10 },
-        { filename: "aab.filter", preset: "VERY-STRICT", level: 20 },
-        { filename: "aac.filter", preset: "UBER-STRICT", level: 30 },
-        { filename: "baa.filter", preset: "General", level: 10 },
-        { filename: "bab.filter", preset: "Strict", level: 20 },
-        { filename: "bac.filter", preset: "Uberstrict", level: 30 },
-      ],
-    },
-  ],
-  (filterGroup) => filterGroup.presets
+	[
+		// ...FILTER_BLAST, // RIP filterblast.xyz ?
+		// { presets: GGG_FILTERS },
+		{
+			presets: [
+				// Neversink
+				{ filename: "ns1.filter", preset: "SEMI-STRICT", level: 10 },
+				{ filename: "ns2.filter", preset: "VERY-STRICT", level: 20 },
+				{ filename: "ns3.filter", preset: "UBER-STRICT", level: 30 },
+				// StupidFatHobbit https://www.pathofexile.com/forum/view-thread/1595976
+				{ filename: "sov1.filter", preset: "General", level: 10 },
+				{ filename: "sov2.filter", preset: "Strict", level: 20 },
+				{ filename: "sov3.filter", preset: "Uberstrict", level: 30 },
+			],
+		},
+	],
+	(filterGroup) => filterGroup.presets
 );
 
 const fetchAndSaveFilter = async (fullUrl, filename) => {
-  const res = await fetch(fullUrl);
-  const filterText = await res.text();
-  writeFileSync(resolve(".", "base-filters", filename), filterText);
+	const res = await fetch(fullUrl);
+	const filterText = await res.text();
+	writeFileSync(resolve(".", "base-filters", filename), filterText);
 };
 
 const fetchGggFilter = async (
-  poesessid,
-  { filename, preset, filterUrl, level }
+	poesessid,
+	{ filename, preset, filterUrl, level }
 ) => {
-  if (poesessid) {
-    try {
-      console.info(`Fetching ${preset}`);
-      const res = await fetch(filterUrl, {
-        credentials: "include",
-        headers: {
-          Accept:
-            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-          "Accept-Language": "en-GB,en;q=0.5",
-          Cookie: `POESESSID=${poesessid}`,
-          "Upgrade-Insecure-Requests": "1",
-          "Sec-Fetch-Dest": "document",
-          "Sec-Fetch-Mode": "navigate",
-          "Sec-Fetch-Site": "same-origin",
-          "Sec-Fetch-User": "?1",
-          Pragma: "no-cache",
-          "Cache-Control": "no-cache",
-        },
-        method: "GET",
-        mode: "cors",
-      });
+	if (poesessid) {
+		try {
+			console.info(`Fetching ${preset}`);
+			const res = await fetch(filterUrl, {
+				credentials: "include",
+				headers: {
+					Accept:
+						"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+					"Accept-Language": "en-GB,en;q=0.5",
+					Cookie: `POESESSID=${poesessid}`,
+					"Upgrade-Insecure-Requests": "1",
+					"Sec-Fetch-Dest": "document",
+					"Sec-Fetch-Mode": "navigate",
+					"Sec-Fetch-Site": "same-origin",
+					"Sec-Fetch-User": "?1",
+					Pragma: "no-cache",
+					"Cache-Control": "no-cache",
+				},
+				method: "GET",
+				mode: "cors",
+			});
 
-      const resText = (await res.text())
-        .replace(/^.*<pre.*class="item-filter-view-filter">/s, "")
-        .replace(/<\/pre>.*$/s, "")
-        .replaceAll("&lt;", "<")
-        .replaceAll("&quot;", '"')
-        .replaceAll("&gt;", ">")
-        .replaceAll("&#039;", "'");
+			const resText = (await res.text())
+				.replace(/^.*<pre.*class="item-filter-view-filter">/s, "")
+				.replace(/<\/pre>.*$/s, "")
+				.replaceAll("&lt;", "<")
+				.replaceAll("&quot;", '"')
+				.replaceAll("&gt;", ">")
+				.replaceAll("&#039;", "'");
 
-      if (resText.includes("!DOCTYPE")) {
-        console.log("resText", resText);
-        throw new Error("It looks like your POESESSID has expired");
-      }
-      writeFileSync(
-        resolve(".", "base-filters", `${level}${filename}`),
-        resText
-      );
-      console.info(`${preset} snippet`, resText.slice(0, 32));
-    } catch (e) {
-      console.error(`Error when fetching ${preset}`);
-      console.error("Your POESESSID may have expired.");
-      console.error(e);
-      return 1;
-    }
+			if (resText.includes("!DOCTYPE")) {
+				console.log("resText", resText);
+				throw new Error("It looks like your POESESSID has expired");
+			}
+			writeFileSync(
+				resolve(".", "base-filters", `${level}${filename}`),
+				resText
+			);
+			console.info(`${preset} snippet`, resText.slice(0, 32));
+		} catch (e) {
+			console.error(`Error when fetching ${preset}`);
+			console.error("Your POESESSID may have expired.");
+			console.error(e);
+			return 1;
+		}
 
-    return 0;
-  }
+		return 0;
+	}
 };
 
 export const fetchAndSaveFilters = async () => {
-  console.info("Removing old filters");
-  try {
-    rmSync(resolve(".", "base-filters"), { recursive: true });
-    mkdirSync(resolve(".", "base-filters"));
-  } catch (e) {
-    console.log("e", e);
-  }
+	console.info("Removing old filters");
+	try {
+		rmSync(resolve(".", "base-filters"), { recursive: true });
+		mkdirSync(resolve(".", "base-filters"));
+	} catch (e) {
+		console.log("e", e);
+	}
 
-  mkdirSync(resolve(".", "base-filters"), { recursive: true });
+	mkdirSync(resolve(".", "base-filters"), { recursive: true });
 
-  console.info("Fetching filters");
-  // RIP filterblast.xyz ?
-  // for (const filter of FILTER_BLAST) {
-  // 	for (const preset of filter.presets) {
-  // 		console.info(`Fetching ${filter.group} ${preset.preset}`);
-  // 		const presetQuery = preset.preset ? `&preset=${preset.preset}` : "";
-  // 		const fullUrl = `${filter.url}${presetQuery}`;
-  // 		await fetchAndSaveFilter(fullUrl, `${preset.level}${preset.filename}`);
-  // 	}
-  // }
+	console.info("Fetching filters");
+	// RIP filterblast.xyz ?
+	// for (const filter of FILTER_BLAST) {
+	// 	for (const preset of filter.presets) {
+	// 		console.info(`Fetching ${filter.group} ${preset.preset}`);
+	// 		const presetQuery = preset.preset ? `&preset=${preset.preset}` : "";
+	// 		const fullUrl = `${filter.url}${presetQuery}`;
+	// 		await fetchAndSaveFilter(fullUrl, `${preset.level}${preset.filename}`);
+	// 	}
+	// }
 
-  // if (process.env.POESESSID) {
-  // 	for (const filter of GGG_FILTERS) {
-  // 		await fetchGggFilter(process.env.POESESSID, filter);
-  // 	}
-  // }
+	// if (process.env.POESESSID) {
+	// 	for (const filter of GGG_FILTERS) {
+	// 		await fetchGggFilter(process.env.POESESSID, filter);
+	// 	}
+	// }
 
-  [
-    // Neversink
-    "10aaa.filter",
-    "20aab.filter",
-    "30aac.filter",
-    // StupidFatHobbit
-    "10baa.filter",
-    "20bab.filter",
-    "30bac.filter",
-  ].forEach((f) => {
-    console.info(`Copying ${f} custom base filter`);
-    copyFileSync(
-      resolve(".", "custom-base-filters", f),
-      resolve(".", "base-filters", f)
-    );
-  });
+	[...presets.map((a) => `${a.level}-${a.filename}`)].forEach((f) => {
+		console.info(`Copying ${f} custom base filter`);
+		copyFileSync(
+			resolve(".", "custom-base-filters", f),
+			resolve(".", "base-filters", f)
+		);
+	});
 
-  console.info("Finished fetching filters");
+	console.info("Finished fetching filters");
 };
